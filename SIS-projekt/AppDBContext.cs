7890309807ffbe0f4a8f34d4058f7756a8d274f4
dpiskovic21 <dpiskovic21@foi.hr>
2024-12-01 15:27:43 +0100

    Add project files.

---
+++ SIS-projekt/AppDBContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIS_projekt.Models;
+
+namespace SIS_projekt
+{
+    public class AppDBContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppDBContext(DbContextOptions<AppDBContext> options, IConfiguration configuration) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            _configuration = configuration;
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>(en =>
+            {
+                en.HasKey(e => e.Id);
+                en.Property(user => user.Email).IsRequired().HasMaxLength(100);
+                en.HasIndex(user => user.Email).IsUnique();
+                en.Property(user => user.Password).IsRequired();
+            });
+        }
+
+
+    }
+}
